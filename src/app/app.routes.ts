e820aa03e01d2a80c0c9d09c05a7091739f3e2ae
khanjandave <khanjancentennial@gmail.com>
2026-09-15 17:50:07 -0400

    changes in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { TransactionHistoryComponent } from './features/transaction-history/transaction-history.component';
 import { CreateTransactionComponent } from './features/create-transaction/create-transaction.component';
 import { authGuard } from './core/guards/auth.guard';
+import { transactionsReducer } from './store/transactions/transactions.reducer';
+import { TransactionsEffects } from './store/transactions/transactions.effects';
 
 export const routes: Routes = [
-  { path: '', component: TransactionHistoryComponent, canActivate: [authGuard] },
-  { path: 'new', component: CreateTransactionComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    providers: [
+      provideState('transactions', transactionsReducer),
+      provideEffects([TransactionsEffects]),
+    ],
+    children: [
+      { path: '', component: TransactionHistoryComponent, canActivate: [authGuard] },
+      { path: 'new', component: CreateTransactionComponent, canActivate: [authGuard] },
+    ],
+  },
 ];
